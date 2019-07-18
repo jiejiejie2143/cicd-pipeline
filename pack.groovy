908afc17f9ae78412c167570fc9d30049cb9b7c6
yanjie <jie.yan@changhong.com>
2019-07-18 07:35:20 +0000

    更新 pack.groovy

--- pack.groovy
+++ pack.groovy
@@ -2,14 +2,19 @@ pipeline {
     agent any
     
     stages {
-        stage('拉取gitlab') {
+        stage('拉取gitlab代码') {
             tools {
                 maven 'maven3.6.1'
                 jdk 'jdk8_161'
             }
-        steps {
-            println('hello world!!!')
+            steps {
+                println('hello world!!!')
+            }
         }
+        stage('打包') {
+            steps {
+                echo '打包命令：mvn -Dmaven.test.skip=true clean package'
+            }
         }
     }
 }
