c9091d222ae4a3eadebffeeb93acd5b6a368a07a
蒋承 <cheng2.jiang@changhong.com>
2019-07-24 10:19:50 +0000

    更新 pack.groovy

--- pack.groovy
+++ pack.groovy
@@ -42,7 +42,6 @@ pipeline {
                     dir(env.ci_dir) {
                         echo "开始docker打包"
                         sh 'bash ../init.sh'+' '+env.appinfo+' '+env.app_name
-                        sh 'mv ../Dockerfile .'
                         sh 'docker build -t registry.cn-hangzhou.aliyuncs.com/ml_test/'+env.app_name+':'+env.BUILD_NUMBER+' .'
                     }
                 }
