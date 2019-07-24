effc8cbdf4a091ca93ab87a5d6b8fffea9a94250
蒋承 <cheng2.jiang@changhong.com>
2019-07-24 09:55:31 +0000

    增加新文件

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# $1 jar或war类型  $2 app名称
+if [ $1 == 'jar' ]
+  then
+    echo 'jar'
+    cat > startup.sh <<EOF
+#!/bin/bash
+java -jar -Dforce-dev=true /data/apps/${2}.jar
+tail -f /dev/null
+EOF
+    cat > Dockerfile <<EOF
+FROM registry.cn-hangzhou.aliyuncs.com/ml_test/centos_jdk:1.0
+COPY  target/${2}.jar /data/apps/${2}.jar
+CMD ["/bin/bash /data/startup.sh"]
+EOF
+    chmod +x startup.sh
+elif [ $1 == 'war' ]
+  then
+    echo 'war'
+    cat > startup.sh <<EOF
+#!/bin/bash
+/bin/bash /data/tomcat8/bin/startup.sh
+tail -f /dev/null
+EOF
+    cat > Dockerfile <<EOF
+FROM registry.cn-hangzhou.aliyuncs.com/ml_test/centos_jdk:1.0
+COPY  target/${2}.war /data/tomcat8/webapps/${2}.war
+CMD ["/bin/bash /data/startup.sh"]
+EOF
+    chmod +x startup.sh
+else
+    echo 'error'
+fi
