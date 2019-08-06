a56ed0b671763efd1485b60cad6daddf1d0f5a06
jiangcheng <cheng2.jiang@changhong.com>
2019-08-06 16:56:59 +0800

    更新

--- host.groovy
+++ host.groovy
@@ -49,7 +49,7 @@ pipeline {
                         env_addr = env_addr.tokenize('\n')[0]
                         echo env_appinfo
                         echo env_apollo
-                        echo env_addr+
+                        echo env_addr
                         if (env_appinfo == 'war') {
                             source_Files = 'target/'+env.app_name+'.war'
                         } else if (env_appinfo == 'jar')  {
@@ -58,8 +58,8 @@ pipeline {
                             error env_appinfo+'项目参数错误，请检查打包的语言类型'
                         }
                         echo source_Files
-                        //def cmd_exe = 'ls /data'
-                        sshPublisher(publishers: [sshPublisherDesc(configName: '114.55.42.166--jenkins_proxy（admin）', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''ls /data''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: env.project, remoteDirectorySDF: false, removePrefix: 'target/', sourceFiles: source_Files)], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+                        def cmd_exe = 'ls /data'
+                        sshPublisher(publishers: [sshPublisherDesc(configName: '114.55.42.166--jenkins_proxy（admin）', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''$cmd_exe''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: env.project, remoteDirectorySDF: false, removePrefix: 'target/', sourceFiles: source_Files)], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
                     }
                 }
             }
