95a5b78872445e2d0c53b0fc3f56ab26a1418773
jiangcheng <cheng2.jiang@changhong.com>
2019-08-06 12:16:37 +0800

    更新gitlab秘钥

--- host.groovy
+++ host.groovy
@@ -13,9 +13,9 @@ pipeline {
                     sh 'mkdir -pv '+env.ci_dir
                     
                     def git_repository = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_program|awk -F "=" \'{print $2}\''
-                    def env_appinfo = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_appinfo|awk -F "=" \'{print $2}\''
-                    def env_apollo = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_apollo|awk -F "=" \'{print $2}\''
-                    def env_addr = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_addr|awk -F "=" \'{print $2}\''
+                    def self_appinfo = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_appinfo|awk -F "=" \'{print $2}\''
+                    def self_apollo = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_apollo|awk -F "=" \'{print $2}\''
+                    def self_addr = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_addr|awk -F "=" \'{print $2}\''
                     def git_branch = env.branch
                     
                     dir(env.ci_dir) {
@@ -44,9 +44,12 @@ pipeline {
                 script {
                     dir(env.ci_dir) {
                         echo "文件传输至跳板机"
-                        echo env_appinfo
-                        echo env_apollo
-                        echo env_addr
+                        def self_appinfo = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_appinfo|awk -F "=" \'{print $2}\''
+                        def self_apollo = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_apollo|awk -F "=" \'{print $2}\''
+                        def self_addr = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_addr|awk -F "=" \'{print $2}\''
+                        echo self_appinfo
+                        echo self_apollo
+                        echo self_addr
                     }
                 }
             }
