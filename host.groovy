e54f55c404ceaea38bedcf9a0df1705f86f82a20
jiangcheng <cheng2.jiang@changhong.com>
2019-08-06 13:10:32 +0800

    更新

--- host.groovy
+++ host.groovy
@@ -46,6 +46,7 @@ pipeline {
                         echo env.appinfo
                         echo env.apollo
                         echo env.addr
+                        sshPublisher(publishers: [sshPublisherDesc(configName: '114.55.42.166--jenkins_proxy（admin）', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''ls /data''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: env.project, remoteDirectorySDF: false, removePrefix: 'target/', sourceFiles: 'target/'+env.app_name+'.jar')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
                 }
             }
         }
