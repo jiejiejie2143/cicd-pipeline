dc5c7ebfc680e36d48e490c175dbbdc79736e264
jiangcheng <cheng2.jiang@changhong.com>
2019-08-06 15:26:22 +0800

    更新

--- host.groovy
+++ host.groovy
@@ -13,9 +13,7 @@ pipeline {
                     sh 'mkdir -pv '+env.ci_dir
                     
                     def git_repository = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_program|awk -F "=" \'{print $2}\''
-                    env.appinfo = sh returnStdout: true, script: 'cat programs/'+env.project+'/program_paras|grep '+env.app_name+'_appinfo|awk -F "=" \'{print $2}\''
-                    env.apollo = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_apollo|awk -F "&" \'{print $2}\''
-                    env.addr = sh returnStdout: true, script: 'cat programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_addr|awk -F "=" \'{print $2}\''
+
                     def git_branch = env.branch
                     
                     dir(env.ci_dir) {
@@ -44,15 +42,18 @@ pipeline {
                 script {
                     dir(env.ci_dir) {
                         echo "文件传输至跳板机"
-                        echo env.appinfo
-                        echo env.apollo
-                        echo env.addr
-                        if (env.appinfo == 'war') {
+                        def env_appinfo = sh returnStdout: true, script: 'cat ../programs/'+env.project+'/program_paras|grep '+env.app_name+'_appinfo|awk -F "=" \'{print $2}\''
+                        def env_apollo = sh returnStdout: true, script: 'cat ../programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_apollo|awk -F "&" \'{print $2}\''
+                        def env_addr = sh returnStdout: true, script: 'cat ../programs/'+env.project+'/'+env.appenv+'_paras|grep '+env.app_name+'_addr|awk -F "=" \'{print $2}\''
+                        echo env_appinfo
+                        echo env_apollo
+                        echo env_addr
+                        if (env_appinfo == 'war') {
                             source_Files = 'target/'+env.app_name+'.war'
-                        } else if (env.appinfo == 'jar')  {
+                        } else if (env_appinfo == 'jar')  {
                             source_Files = 'target/'+env.app_name+'.jar'
                         } else {
-                            error env.appinfo+'项目参数错误，请检查打包的语言类型'
+                            error env_appinfo+'项目参数错误，请检查打包的语言类型'
                         }
                         echo source_Files
                         sshPublisher(publishers: [sshPublisherDesc(configName: '114.55.42.166--jenkins_proxy（admin）', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''ls /data''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: env.project, remoteDirectorySDF: false, removePrefix: 'target/', sourceFiles: source_Files)], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
