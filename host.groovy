2dc72ea708c272f407e12ed967f7f7fef30bdb75
jiangcheng <cheng2.jiang@changhong.com>
2019-08-08 21:17:36 +0800

    方法返回值修改

--- host.groovy
+++ host.groovy
@@ -6,9 +6,9 @@ def getParas(keyword,keyenv = env.appenv) {
     common_paras = sh returnStdout: true, script: 'cat programs/' + env.project + '/'+keyenv+'_paras|grep ' + env.project + '_' + keyword + '|awk -F "=" \'{print $2}\''
     common_paras = common_paras.tokenize('\n')[0]
     if (self_paras != 'null')  {
-        paras = common_paras
-    } else {
         paras = self_paras
+    } else {
+        paras = common_paras
     }
     return paras
 }
