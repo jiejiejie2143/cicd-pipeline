0583705da80c4b7bb9b0a724fa4b16597e6ab55e
jiangcheng <cheng2.jiang@changhong.com>
2019-08-14 17:50:57 +0800

    更改docker启动参数

--- init.sh
+++ init.sh
@@ -12,7 +12,7 @@ EOF
 FROM registry-vpc.cn-hangzhou.aliyuncs.com/ml_test/centos_jdk:1.0
 COPY target/${2}.jar /data/apps/${2}.jar
 COPY startup.sh /data/startup.sh
-CMD ["/bin/bash /data/startup.sh"]
+CMD ["/data/startup.sh"]
 EOF
     chmod +x startup.sh
 elif [ $1 == 'war' ]
@@ -30,7 +30,7 @@ EOF
 FROM registry-vpc.cn-hangzhou.aliyuncs.com/ml_test/centos_jdk:1.0
 COPY target/${2}.war /data/tomcat8/webapps/${2}.war
 COPY startup.sh /data/startup.sh
-CMD ["/bin/bash /data/startup.sh"]
+CMD ["/data/startup.sh"]
 EOF
     chmod +x startup.sh
 else
